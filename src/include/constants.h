7a0c5dc41e58dbcffa29a147f9411b1a004e4598
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2025-02-25 23:34:47 +0700

    Add files via upload

---
+++ src/include/constants.h
@@ -0,0 +1,43 @@
+#ifndef constants_h_
+#define constants_h_
+
+#define WINDOW_TITLE "Tetris game - [Bài tập lớn Lập trình nâng cao]"
+#define SQUARE_BLOCK_SIZE 25
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define RATIO 1.50f
+
+// path to images
+#define BACKGROUND_PATH "./src/images/main-background.png"
+#define LOGO_PATH "./src/images/main-logo.png"
+#define MATRIX_PATH "src/images/matrix.png"
+#define DYING_I_PATH "src/images/dying/I.png"
+#define DYING_J_PATH "src/images/dying/J.png"
+#define DYING_L_PATH "src/images/dying/L.png"
+#define DYING_O_PATH "src/images/dying/O.png"
+#define DYING_S_PATH "src/images/dying/S.png"
+#define DYING_T_PATH "src/images/dying/T.png"
+#define DYING_Z_PATH "src/images/dying/Z.png"
+#define GHOST_I_PATH "src/images/ghost/I.png"
+#define GHOST_J_PATH "src/images/ghost/J.png"
+#define GHOST_L_PATH "src/images/ghost/L.png"
+#define GHOST_O_PATH "src/images/ghost/O.png"
+#define GHOST_S_PATH "src/images/ghost/S.png"
+#define GHOST_T_PATH "src/images/ghost/T.png"
+#define GHOST_Z_PATH "src/images/ghost/Z.png"
+#define LOCKED_I_PATH "src/images/locked/I.png"
+#define LOCKED_J_PATH "src/images/locked/J.png"
+#define LOCKED_L_PATH "src/images/locked/L.png"
+#define LOCKED_O_PATH "src/images/locked/O.png"
+#define LOCKED_S_PATH "src/images/locked/S.png"
+#define LOCKED_T_PATH "src/images/locked/T.png"
+#define LOCKED_Z_PATH "src/images/locked/Z.png"
+#define NORMAL_I_PATH "src/images/normal/I.png"
+#define NORMAL_J_PATH "src/images/normal/J.png"
+#define NORMAL_L_PATH "src/images/normal/L.png"
+#define NORMAL_O_PATH "src/images/normal/O.png"
+#define NORMAL_S_PATH "src/images/normal/S.png"
+#define NORMAL_T_PATH "src/images/normal/T.png"
+#define NORMAL_Z_PATH "src/images/normal/Z.png"
+
+#endif  // constants_h_
---
+++ src/include/utils.hpp
@@ -0,0 +1,15 @@
+#ifndef utils_hpp_
+#define utils_hpp_
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <unordered_map>
+#include <string>
+
+void attachTextureToRenderer(SDL_Texture *texture, SDL_Renderer *renderer, int x, int y);
+void attachTextureToRendererFilled(SDL_Texture *texture, SDL_Renderer *renderer);
+void attachTextureToRendererCentered(SDL_Texture *texture, SDL_Renderer *renderer);
+void loadTexturesToRenderer(std::unordered_map<std::string, SDL_Texture*> &textures, SDL_Renderer *renderer);
+void loadLogo(SDL_Window *window);
+
+#endif  // utils_hpp_
